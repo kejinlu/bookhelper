0e45142a7671f77f321926db3bd5f20d71afc728
luke <kejinlu@gmail.com>
2011-07-13 22:00:22 +0800

    在请求图片的时候，增加activity indicator

--- Libraries/SDWebImage/UIImageView+WebCache.m
+++ Libraries/SDWebImage/UIImageView+WebCache.m
@@ -8,6 +8,7 @@
 
 #import "UIImageView+WebCache.h"
 #import "SDWebImageManager.h"
+#define ACTIVITY_INDICATOR_TAG 100
 
 @implementation UIImageView (WebCache)
 
@@ -22,11 +23,18 @@
 
     // Remove in progress downloader from queue
     [manager cancelForDelegate:self];
-
+	
     self.image = placeholder;
-
     if (url)
     {
+		//如果url存在，则添加等待标示
+		UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+		activityIndicator.center = CGPointMake(self.bounds.size.width/2.0, self.bounds.size.height/2.0);
+		activityIndicator.tag = ACTIVITY_INDICATOR_TAG;
+		[activityIndicator startAnimating];
+		[self addSubview:activityIndicator];
+		[activityIndicator release];
+		
         [manager downloadWithURL:url delegate:self];
     }
 }
@@ -38,6 +46,12 @@
 
 - (void)webImageManager:(SDWebImageManager *)imageManager didFinishWithImage:(UIImage *)image
 {
+	
+	UIActivityIndicatorView *activityIndicator = (UIActivityIndicatorView *)[self viewWithTag:ACTIVITY_INDICATOR_TAG];
+	if (activityIndicator) {
+		[activityIndicator removeFromSuperview];
+	}
+	
     self.image = image;
 }
 
